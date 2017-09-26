a907b347bab5660aaed9d0c34b621aedee61f07f
adamgao1996 <adamgao1996@gmail.com>
2017-09-25 23:41:10 -0400

    init login bot

---
+++ login_bot/src/main/java/adam/gao/login_bot/login_bot/App.java
@@ -0,0 +1,13 @@
+package adam.gao.login_bot.login_bot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
